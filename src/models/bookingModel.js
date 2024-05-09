9318d89ba0137a20bc73651e16e4a4621e494820
Mahmoud Samir <mahmodsamir28@gmail.com>
2024-05-09 19:33:03 +0300

    data modeling

---
+++ src/models/bookingModel.js
@@ -0,0 +1,38 @@
+const { Schema, Types, model } = require('mongoose');
+
+const bookingSchema = new Schema({
+  tour: {
+    type: Types.ObjectId,
+    ref: 'Tour',
+    required: [true, 'Booking must belong to a Tour!'],
+  },
+  user: {
+    type: Types.ObjectId,
+    ref: 'User',
+    required: [true, 'Booking must belong to a User!'],
+  },
+  price: {
+    type: Number,
+    require: [true, 'Booking must have a price.'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  paid: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+bookingSchema.pre(/^find/, function (next) {
+  this.populate('user').populate({
+    path: 'tour',
+    select: 'name',
+  });
+  next();
+});
+
+const Booking = model('Booking', bookingSchema);
+
+module.exports = Booking;
